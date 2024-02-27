834109d4a03206efcd83c5841f9a759015573b55
Henning Lorenzen <henning.lorenzen.ext@bayer.com>
2024-02-27 15:48:07 +0000

    add reprex source file
    https://github.com/kassambara/survminer/issues/643

---
+++ reprex_gg2_350.R
@@ -0,0 +1,26 @@
+library(survival)
+
+data <- survival::lung
+fit1 <- survfit(Surv(time, status) ~ sex, data = data)
+
+# produces warning from ggplot2 3.5.0
+survminer::ggsurvplot(
+  fit = fit1,
+  data = data,
+  surv.median.line = "hv"
+)
+
+# works fine w/o surv.median.line
+survminer::ggsurvplot(
+  fit = fit1,
+  data = data
+)
+
+# works fine with only one suvival curve
+fit2 <- survfit(Surv(time, status) ~ 1, data = data)
+
+survminer::ggsurvplot(
+  fit = fit2,
+  data = data,
+  surv.median.line = "hv"
+)
